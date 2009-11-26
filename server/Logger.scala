9b8713c52a1d050405ef1c5375f3dc3f807c88e5
Etienne Kneuss <etienne@swengsrv.epfl.ch>
2009-11-26 22:56:06 +0100

    Small logger change

--- server/Logger.scala
+++ server/Logger.scala
@@ -16,14 +16,14 @@ class TerminalLogger extends Logger {
 
     def setIP(ip: String) = { this.ip = Some(ip) }
     def getIP = ip match {
-        case Some(s) => "["+s+"]"
+        case Some(s) => "["+s+"] "
         case None => ""
     }
     val df = new SimpleDateFormat("dd-MMM-yy HH:mm:ss")
     def date = df.format(new Date())
 
     def out(msg: String)  = println(date+" "+getIP+"[>] "+msg)
-    def info(msg: String)   = println(date+" "+getIP+"[-] "+msg)
+    def info(msg: String) = println(date+" "+getIP+"[-] "+msg)
     def in(msg: String)   = println(date+" "+getIP+"[<] "+msg)
     def err(msg: String)  = println(date+" "+getIP+"[!] "+msg)
     def warn(msg: String) = println(date+" "+getIP+"[w] "+msg)
